1dc2f33f51e819e921e330b2866695a0edb8526c
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-09-05 01:38:44 +0530

    making public folder available at http
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
-
+const path = require('path')
 const app = express()
+
+//to make public folder available at http request.
+app.use('/',express.static(path.join(__dirname,'../public')))
 
 app.get('/',(req,res) => {
     res.send('Hello world!')
