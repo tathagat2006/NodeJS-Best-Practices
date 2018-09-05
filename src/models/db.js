c6c698543428a4650a7d0ac517175429853b538d
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-09-05 17:43:58 +0530

    user,author and comment db created
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- src/models/db.js
+++ src/models/db.js
@@ -9,6 +9,40 @@ const db = new Sequelize(
     }
 )
 
+const User = db.define('user', {
+    username: {type: DataTypes.STRING(30), unique: true, allowNull: false},
+    passwords: {type: DataTypes.STRING, allowNull: false}
+})
+
+const Article = db.define('article', {
+    title: {
+        type: DataTypes.STRING(50),
+        allowNull: true
+    },
+    content: {
+        type: DataTypes.STRING
+    }
+})
+
+const Comment = db.define('comment', {
+    title: {
+        type: DataTypes.STRING(150),
+        allowNull: true
+    },
+    message: {
+        type: DataTypes.STRING(150)
+    }
+})
+
+Article.belongsTo(User, {as: 'author'})
+User.hasMany(Article)
+
+Comment.belongsTo(Article)
+Article.hasMany(Comment)
+
+Comment.belongsTo(User)
+User.hasMany(Comment)
+
 module.exports = {
     db
 }
